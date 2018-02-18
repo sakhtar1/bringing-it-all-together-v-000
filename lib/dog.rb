b75f93a39b54e8ab4156e8e1578afefa1567c03b
Sharmina Akhtar <sakht318@gmail.com>
2018-02-18 16:00:18 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,70 @@
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(id: nil, :breed, :name)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+
+  def Dog.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP table IF exists dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
+  end
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id, name,breed)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = "UPDATE dog SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+end
