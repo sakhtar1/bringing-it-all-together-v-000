d4d7bf5bd196cb24ed5a01ac78e8be7f1a426bfa
Sharmina Akhtar <sakht318@gmail.com>
2018-02-18 18:06:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,9 +67,17 @@ class Dog
     end.first
   end
 
-  def self.find_by_id(id, name, breed)
-   sql = "SELECT * FROM dogs WHERE id = ?"
-   @id = DB[:conn].execute(sql, id)[0]
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
 
  end
 
